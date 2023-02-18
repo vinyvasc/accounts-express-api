e2301c9158cb0a77167415ebf23616991c44e22c
Vinicius <vinivascdev@gmail.com>
2023-02-18 15:06:26 -0300

    adição do user model

---
+++ models/user.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const sequelize = require('../connection');
+const { DataTypes } = require('sequelize');
+
+const User = sequelize.define('User', {
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true
+    }, 
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    }
+},{
+    schema: 'public',
+    tableName: 'users',
+    timestamps: true,
+    createdAt: 'created_at',
+    updatedAt: 'updated_at',
+    deletedAt: 'deleted_at'    
+});
+
+module.exports = User;
